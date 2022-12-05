8a09e3edbb9e34f45e06dc937cf8c9be0fc494ac
obedi bedjo <obedindevu@gmail.com>
2022-12-05 15:55:00 +0200

    style: user in private chat

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -66,15 +66,15 @@ function Sidebar() {
     }
     return (
         <>
-            <h2>Available rooms</h2>
+            <h2>Groupes</h2>
             <ListGroup>
                 {rooms.map((room, idx) => (
                     <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room == currentRoom} style={{ cursor: "pointer", display: "flex", justifyContent: "space-between" }}>
-                        {room} {currentRoom !== room && <span className="badge rounded-pill bg-primary">{user.newMessages[room]}</span>}
+                        {room} {currentRoom !== room && <span className="badge rounded-pill bg-black">{user.newMessages[room]}</span>}
                     </ListGroup.Item>
                 ))}
             </ListGroup>
-            <h2>Members</h2>
+            <h2>Contacts</h2>
             {members.map((member) => (
                 <ListGroup.Item key={member.id} style={{ cursor: "pointer" }} active={privateMemberMsg?._id == member?._id} onClick={() => handlePrivateMemberMsg(member)} disabled={member._id === user._id}>
                     <Row>
